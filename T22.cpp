abc0a7a8b35b2ce104638824175f0d45cbd662d6
Wangbwm <1259963577@qq.com>
2022-05-17 23:10:44 +0800

    T22

---
+++ T22.cpp
@@ -0,0 +1,36 @@
+/*
+this is a very simple but cool tool write by riot. Use it!It's for our fairness.
+*/
+
+/*
+if u have a game with RNG.Maybe u can try this 
+*/
+#include<iostream>
+#include <time.h> 
+#include <windows.h>
+/*
+T22:a team finishes a game with 1557(IG:T1)
+krdog:someone without mom
+*/
+using namespace std;
+
+int ping_krdog()//a very very clever function for our msi.It's so cool!
+{
+    while(1)
+    {
+       
+        int ping = 32 + rand() % 5; 
+        cout<<"ping:"<<ping<<endl;
+         Sleep(2000);
+
+    }
+}
+
+//krdog,i'll kill u mom
+
+int main()
+{
+    ping_krdog();
+    return 0;
+
+}
